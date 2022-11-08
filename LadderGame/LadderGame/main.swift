0bcd9ae4b46dbf4e041f17f064472c9c4cb5d3ef
Hyejeong Park <effie.thedev@gmail.com>
2022-11-08 17:51:05 +0900

    [Refactor] 구현 개선
    피드백 반영 - 함수 단위로 구현 정리

--- LadderGame/LadderGame/main.swift
+++ LadderGame/LadderGame/main.swift
@@ -7,37 +7,47 @@
 
 import Foundation
 
-let input = readLine() ?? ""
-let numberOfPlayers = Int(input.trimmingCharacters(in: .whitespacesAndNewlines)) ?? 0
-if numberOfPlayers < 2 || numberOfPlayers > 8 { exit(1) }
-
-let ladderHeight = 4
-
-// [[가로], [가로], [가로], ...] 2차원 배열
-// 모두 0에서 값을 변경하는 방식
-var rows: [[Int]] = Array(repeating: Array(repeating: 0, count: numberOfPlayers - 1), count: ladderHeight)
-
-// 열 기준으로 돌림
-for col in 0 ..< numberOfPlayers - 1 {
-  // [세로] 하나의 열을 생성하고 -> 각 열에 대입
-  var column = Array(repeating: 0, count: ladderHeight)
+func getNumberOfPlayers(between lowerBound: Int, and upperBound: Int) -> Int {
+  var numberOfPlayers: Int
   repeat {
-    for row in 0 ..< rows.count {
-      // 두 번째 열부터는 왼쪽에 발판이 이미 있다면 오른쪽은 없어야
-      if col > 0 && rows[row][col - 1] == 1 { column[row] = 0 }
-      // 아니면 랜덤
-      else { column[row] = Int.random(in: 0 ... 1) }
-    }
-  } while column.filter { $0 == 1 }.count == 0 // 열에 발판이 없으면 다시 반복
+    print("알맞은 사다리게임 플레이어 수를 입력하세요 (\(lowerBound)~\(upperBound)명):", terminator: " ")
+    let input = readLine() ?? ""
+    numberOfPlayers = Int(input.trimmingCharacters(in: .whitespacesAndNewlines)) ?? 0
+  } while numberOfPlayers < lowerBound || numberOfPlayers > upperBound
+  return numberOfPlayers
+}
+
+func randomizeSteps(ladderHeight: Int, numberOfPlayers: Int) -> [[Int]] {
+  var rows: [[Int]] = Array(repeating: Array(repeating: 0, count: numberOfPlayers - 1),
+                            count: ladderHeight)
   
-  for (row, hasStep) in column.enumerated() {
-    rows[row][col] = hasStep
+  for col in 0 ..< numberOfPlayers - 1 {
+    var column = Array(repeating: 0, count: ladderHeight)
+    repeat {
+      for row in 0 ..< column.count {
+        if col > 0 && rows[row][col - 1] == 1 { column[row] = 0 }
+        else { column[row] = Int.random(in: 0 ... 1) }
+      }
+    } while column.filter { $0 == 1 }.count == 0
+    
+    for (row, hasStep) in column.enumerated() {
+      rows[row][col] = hasStep
+    }
+  }
+  
+  return rows
+}
+
+/// 주어진 발판 배열에 따라 사다리를 출력합니다.
+/// - Parameter rows: 행별 발판 정보
+func printLadderSteps(with rowsOfSteps: [[Int]]) {
+  rowsOfSteps.forEach { row in
+    print("|", terminator: "")
+    row.forEach { step in print(step == 1 ? "-": " ", terminator: "|") }
+    print()
   }
 }
 
-// 출력
-rows.forEach {
-  print("|", terminator: "")
-  $0.forEach { print($0 == 0 ? " ": "-", terminator: "|") }
-  print()
-}
+let numberOfPlayers = getNumberOfPlayers(between: 2, and: 8)
+let stepArray = randomizeSteps(ladderHeight: 4, numberOfPlayers: numberOfPlayers)
+printLadderSteps(with: stepArray)
