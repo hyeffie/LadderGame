6441b50b64e081f1818b07b12f9758ebeae2ca44
Hyejeong Park <effie.thedev@gmail.com>
2022-11-08 16:40:06 +0900

    [Rename, Comment] 변수명 변경, 코드 정리 및 주석 추가

--- LadderGame/LadderGame/main.swift
+++ LadderGame/LadderGame/main.swift
@@ -12,26 +12,31 @@ let numberOfPlayers = Int(input.trimmingCharacters(in: .whitespacesAndNewlines))
 if numberOfPlayers < 2 || numberOfPlayers > 8 { exit(1) }
 
 let ladderHeight = 4
-var spaces: [[Int]] = Array(repeating: Array(repeating: 0, count: numberOfPlayers - 1), count: ladderHeight)
 
-for i in 0 ..< numberOfPlayers - 1 {
-  var hSpaces = Array(repeating: 0, count: ladderHeight)
+// [[가로], [가로], [가로], ...] 2차원 배열
+// 모두 0에서 값을 변경하는 방식
+var rows: [[Int]] = Array(repeating: Array(repeating: 0, count: numberOfPlayers - 1), count: ladderHeight)
+
+// 열 기준으로 돌림
+for col in 0 ..< numberOfPlayers - 1 {
+  // [세로] 하나의 열을 생성하고 -> 각 열에 대입
+  var column = Array(repeating: 0, count: ladderHeight)
   repeat {
-    for (index, _) in hSpaces.enumerated() {
-      if i > 0 && spaces[index][i - 1] == 1 {
-        hSpaces[index] = 0
-      } else {
-        hSpaces[index] = Int.random(in: 0 ... 1)
-      }
+    for row in 0 ..< rows.count {
+      // 두 번째 열부터는 왼쪽에 발판이 이미 있다면 오른쪽은 없어야
+      if col > 0 && rows[row][col - 1] == 1 { column[row] = 0 }
+      // 아니면 랜덤
+      else { column[row] = Int.random(in: 0 ... 1) }
     }
-  } while hSpaces.filter { $0 == 1 }.count == 0
+  } while column.filter { $0 == 1 }.count == 0 // 열에 발판이 없으면 다시 반복
   
-  for (index, number) in hSpaces.enumerated() {
-    spaces[index][i] = number
+  for (row, hasStep) in column.enumerated() {
+    rows[row][col] = hasStep
   }
 }
 
-spaces.forEach {
+// 출력
+rows.forEach {
   print("|", terminator: "")
   $0.forEach { print($0 == 0 ? " ": "-", terminator: "|") }
   print()
